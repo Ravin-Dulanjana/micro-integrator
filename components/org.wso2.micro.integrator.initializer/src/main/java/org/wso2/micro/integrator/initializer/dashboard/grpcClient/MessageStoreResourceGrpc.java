d894af662c43186e768f7c1d3b7da13d5e6e428a
Ravin Fernando <ravinf@wso2.com>
2023-06-08 16:51:06 +0530

    Adds MessageStoreResourceGrpc

---
+++ components/org.wso2.micro.integrator.initializer/src/main/java/org/wso2/micro/integrator/initializer/dashboard/grpcClient/MessageStoreResourceGrpc.java
@@ -0,0 +1,117 @@
+package org.wso2.micro.integrator.initializer.dashboard.grpcClient;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.config.SynapseConfigUtils;
+import org.apache.synapse.config.SynapseConfiguration;
+import org.apache.synapse.config.xml.MessageStoreSerializer;
+import org.apache.synapse.message.store.MessageStore;
+import org.apache.synapse.message.store.impl.jdbc.JDBCMessageStore;
+import org.apache.synapse.message.store.impl.jms.JmsStore;
+import org.apache.synapse.message.store.impl.memory.InMemoryStore;
+import org.apache.synapse.message.store.impl.rabbitmq.RabbitMQStore;
+import org.apache.synapse.message.store.impl.resequencer.ResequenceMessageStore;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.wso2.carbon.inbound.endpoint.common.Constants.SUPER_TENANT_DOMAIN_NAME;
+import static org.wso2.micro.integrator.initializer.dashboard.grpcClient.Constants.PASSWORD;
+import static org.wso2.micro.integrator.initializer.dashboard.grpcClient.Constants.PASSWORD_MASKED_VALUE;
+
+public class MessageStoreResourceGrpc {
+
+    private static final Log LOG = LogFactory.getLog(MessageStoreResourceGrpc.class);
+    private static final String JDBCMESSAGESTORE_TYPE = "jdbc-message-store";
+    private static final String JMSSTORE_TYPE = "jms-message-store";
+    private static final String INMEMORYSTORE_TYPE = "in-memory-message-store";
+    private static final String RABBITMQSTORE_TYPE = "rabbitmq-message-store";
+    private static final String RESEQUENCEMESSAGESTORE_TYPE = "resequence-message-store";
+    private static final String CUSTOMSTORE_TYPE = "custom-message-store";
+    //Constants for message-processor JSON object
+    private static final String STORE_TYPE_PROPERTY = "type";
+    private static final String CONTAINER_ATTRIBUTE = "container";
+    private static final String FILE_NAME_ATTRIBUTE = "file";
+    private static final String PROPERTIES_ATTRIBUTE = "properties";
+    private static final String STORE_SIZE_ATTRIBUTE = "size";
+    private List<MessageStore> getSearchResults(String searchKey) {
+        SynapseConfiguration configuration = SynapseConfigUtils.getSynapseConfiguration(SUPER_TENANT_DOMAIN_NAME);
+        return configuration.getMessageStores().values().stream()
+                .filter(artifact -> artifact.getName().toLowerCase().contains(searchKey))
+                .collect(Collectors.toList());
+    }
+
+    private void populateSearchResults(String searchKey) {
+
+        List<MessageStore> searchResultList = getSearchResults(searchKey);
+        setGrpcResponseBody(searchResultList);
+    }
+
+    private void setGrpcResponseBody(Collection<MessageStore> storeList) {
+
+        org.wso2.micro.integrator.grpc.proto.MessageStoreList.Builder messageStoreListBuilder = org.wso2.micro.integrator.grpc.proto.MessageStoreList.newBuilder().setCount(storeList.size());
+
+        for (MessageStore store : storeList) {
+            messageStoreListBuilder.addMessageStoreSummaries(getProtoMessageStore(store));
+        }
+        //Utils.setJsonPayLoad(axis2MessageContext, jsonBody);
+    }
+
+    private void populateMessageStoreList(SynapseConfiguration synapseConfiguration) {
+        Map<String, MessageStore> storeMap = synapseConfiguration.getMessageStores();
+        Collection<MessageStore> storeList = storeMap.values();
+        setGrpcResponseBody(storeList);
+    }
+
+    private void populateMessageStoreData(org.apache.axis2.context.MessageContext axis2MessageContext,
+                                          SynapseConfiguration synapseConfiguration,
+                                          String messageStoreName) {
+        MessageStore messageStore = synapseConfiguration.getMessageStore(messageStoreName);
+        if (Objects.nonNull(messageStore)) {
+            org.wso2.micro.integrator.grpc.proto.MessageStore messageStorePB = getMessageStoreAsProtoBuf(messageStore);
+            //send message processor data
+        } else {
+            LOG.warn("Message store " + messageStoreName + " does not exist");
+            GrpcUtils.createProtoError("Specified message store ('" + messageStoreName + "') not found");
+        }
+    }
+
+    private org.wso2.micro.integrator.grpc.proto.MessageStoreSummary getProtoMessageStore(MessageStore messageStore) {
+        org.wso2.micro.integrator.grpc.proto.MessageStoreSummary.Builder messageStoreSummaryBuilder = org.wso2.micro.integrator.grpc.proto.MessageStoreSummary.newBuilder();
+        messageStoreSummaryBuilder.setName(messageStore.getName());
+        messageStoreSummaryBuilder.setType(getStoreType(messageStore));
+        messageStoreSummaryBuilder.setSize(messageStore.size());
+        return messageStoreSummaryBuilder.build();
+    }
+
+    private String getStoreType(MessageStore messageStore) {
+
+        if (messageStore instanceof ResequenceMessageStore) {
+            return RESEQUENCEMESSAGESTORE_TYPE;
+        } else if (messageStore instanceof JDBCMessageStore) {
+            return JDBCMESSAGESTORE_TYPE;
+        } else if (messageStore instanceof JmsStore) {
+            return JMSSTORE_TYPE;
+        } else if (messageStore instanceof InMemoryStore) {
+            return INMEMORYSTORE_TYPE;
+        } else if (messageStore instanceof RabbitMQStore) {
+            return RABBITMQSTORE_TYPE;
+        } else {
+            return CUSTOMSTORE_TYPE;
+        }
+    }
+
+    private org.wso2.micro.integrator.grpc.proto.MessageStore getMessageStoreAsProtoBuf(MessageStore messageStore) {
+        org.wso2.micro.integrator.grpc.proto.MessageStore.Builder messageStoreBuilder = org.wso2.micro.integrator.grpc.proto.MessageStore.newBuilder();
+        messageStoreBuilder.setName(messageStore.getName());
+        messageStoreBuilder.setContainer(messageStore.getArtifactContainerName());
+        messageStoreBuilder.setFileName(messageStore.getFileName());
+        messageStoreBuilder.setStoreSize(messageStore.size());
+        messageStoreBuilder.setConfiguration(MessageStoreSerializer.serializeMessageStore(null, messageStore, true).toString());
+        //parameters
+
+        return messageStoreBuilder.build();
+    }
+}
